4c073b80ba9dfbfafa89113ab0d4405ed4ab9fd9
sanqi377 <sanqi377@outlook.com>
2021-03-29 15:00:58 +0800

    updata

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
 wx.$util = require("./utils/util");
 App({
     // 全局变量
-    collect:{
-        data:[]
+    collect: {
+        data: []
     },
     /**
      * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
      */
-    onLaunch: function () {
-<<<<<<< HEAD
-        // wx.login({
-        //     success(res) {
-        //         if (res.code) {
-        //             //发起网络请求
-        //             wx.request({
-        //                 url: 'https://test.com/onLogin',
-        //                 data: {
-        //                     code: res.code
-        //                 }
-        //             })
-        //         } else {
-        //             console.log('登录失败！' + res.errMsg)
-        //         }
-        //     }
-        // })
-=======
-
->>>>>>> b11b41765d67a23e5707208b92a12a3eaf72666b
-    },
+    onLaunch: function () {},
 
     /**
      * 当小程序启动，或从后台进入前台显示，会触发 onShow
