c293b6046f00407ebd184b3be982cb57df420a17
sanqi377 <sanqi377@outlook.com>
2021-04-06 19:42:38 +0800

    updata

--- config/cofig.js
+++ config/cofig.js
@@ -1,9 +1,9 @@
 export default {
     // tabs: ['home', 'article', 'profile'],
     // 远端地址，上线切换
-    // serverPath: 'http://svanrj.ltd:88/',
+    serverPath: 'https://xb.caozhuti.com',
     // 本地地址
-    serverPath: 'http://127.0.0.1:3000/',
+    // serverPath: 'http://127.0.0.1:3000/',
     // 图片服务器
     // imagePath: 'https://xxx.com/xximg/',
   }
